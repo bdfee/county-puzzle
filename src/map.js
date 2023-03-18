01a3b4ae35a0edff72ce21a6c45af036c4e14563
ben durfee <73133492+bdfee@users.noreply.github.com>
2023-03-18 19:41:39 -0400

    Tooltip (#3)
    * initial tooltip implementation
    
    * tool tip dissapears on drag, still an issue with other county tips appearing beneath the tooltip
    
    * tool tip
    
    * tool tip implemented

--- src/map.js
+++ src/map.js
@@ -15,7 +15,10 @@ function USMap() {
   const mapRef = useRef()
   const [topology, setTopology] = useState(null)
   const [piecesLocated, setPiecesLocated] = useState({})
+  const [tooltipText, setTooltipText] = useState('')
+  const [tooltipCoords, setTooltipCoords] = useState([])
 
+  const tooltipStyle = { left: tooltipCoords[0], top: tooltipCoords[1] }
   // check if pieceLocated obj needs updating, and update
   const updatePieceLocated = (id, bool) => {
     const state = id.substring(0, 2)
@@ -88,12 +91,60 @@ function USMap() {
     getTopology()
   }, [])
 
+  // handlers
+  const dragHandler = d3
+    .drag()
+    .on('start', function () {
+      setTooltipText('')
+      // class active to bring to the top, remove tooltip
+      d3.select(this).raise().classed('active', true)
+      // remove tool tip on other counties
+      d3.selectAll('.county').attr('pointer-events', 'none')
+    })
+    .on('drag', function ({ dx, dy }) {
+      // Get the current transform value
+      const transform = d3.select(this).node().transform.baseVal[0].matrix
+      const changeX = transform.e
+      const changeY = transform.f
+      // add the new translation values to dx
+      d3.select(this).attr('transform', `translate(${changeX + dx},${changeY + dy})`)
+    })
+    .on('end', function (d) {
+      d3.select(this).classed('active', false)
+      // add tool tips
+      d3.selectAll('.county').attr('pointer-events', 'all')
+      // if county translate is 0 0, it is located correctly
+      const isLocated = d3.select(this).attr('transform') === 'translate(0,0)'
+      if (isLocated) {
+        // remove drag handler and adjust stroke style when correctly located
+        d3.select(this)
+          .classed('located', true)
+          .attr('stroke-width', 0.1)
+          .attr('stroke', 'lightgray')
+          .on('.drag', null)
+      }
+      updatePieceLocated(d.subject.id, isLocated)
+    })
+
+  function handleMouseOver(e, d) {
+    setTooltipCoords([e.clientX, e.clientY])
+    setTooltipText(d.properties.name)
+  }
+
+  function handleMouseMove(e) {
+    setTooltipCoords([e.clientX, e.clientY])
+  }
+
+  function handleMouseOut() {
+    setTooltipText('')
+  }
+
   useEffect(() => {
     // remove any svg el from previous render
     d3.select(mapRef.current).selectAll('*').remove()
 
-    const width = window.outerWidth
-    const height = window.outerHeight
+    const width = window.innerWidth
+    const height = window.innerHeight
 
     // geoAlbersUSA projection, center on window/svg
     const projection = d3
@@ -104,8 +155,13 @@ function USMap() {
     // Create a path generator that converts GeoJSON geometries to SVG path elements
     const pathGenerator = d3.geoPath().projection(projection)
 
-    const svg = d3.select(mapRef.current).append('svg').attr('width', width).attr('height', height)
-    svg.attr('viewBox', `0 0 ${width} ${height}`)
+    const svg = d3
+      .select(mapRef.current)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('viewBox', `0 0 ${width} ${height}`)
+
     // todo improve color randomization
     const stateColorScale = d3.scaleOrdinal().range(d3.schemeCategory10)
 
@@ -135,40 +191,19 @@ function USMap() {
         .attr('data-state-id', (d) => `state-id-${d.id.slice(0, 2)}`)
         .attr('data-name', (d) => `${d.properties.name}`)
         .attr('transform', () => randomTranslation())
+        .on('mouseover', (e, d) => handleMouseOver(e, d))
+        .on('mousemove', (e) => handleMouseMove(e))
+        .on('mouseout', handleMouseOut)
 
-      const dragHandler = d3
-        .drag()
-        .on('start', function () {
-          // use function so that this is present
-          // class active to bring to the top css
-          d3.select(this).raise().classed('active', true)
-        })
-        .on('drag', function ({ dx, dy }) {
-          // Get the current transform value
-          const transform = d3.select(this).node().transform.baseVal[0].matrix
-          const changeX = transform.e
-          const changeY = transform.f
-          console.log('this', changeX, changeY, dx, dy)
-          // add the new translation values to dx
-          d3.select(this).attr('transform', `translate(${changeX + dx},${changeY + dy})`)
-        })
-        .on('end', function (d) {
-          d3.select(this).classed('active', false)
-          const isLocated = d3.select(this).attr('transform') === 'translate(0,0)'
-          if (isLocated) {
-            d3.select(this)
-              .classed('located', true)
-              .attr('stroke-width', 0.1)
-              .attr('stroke', 'lightgray')
-              .on('.drag', null)
-          }
-          updatePieceLocated(d.subject.id, isLocated)
-        })
       countyPaths.call(dragHandler)
     }
   }, [topology])
+
   return (
     <div>
+      <div className="tooltip" style={tooltipStyle}>
+        {tooltipText.length ? tooltipText : ''}
+      </div>
       <div ref={mapRef}></div>
     </div>
   )
