72bf7e43f9f5b72efd6eb99ee29493fc53365cba
Ben Durfee <ben.p.durfee@gmail.com>
2023-05-02 15:21:56 -0400

    some splitting of svg utils and the rendering logic

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-County Puzzle
+#US County Puzzle
+
+This project uses US county topology data (TopoJSON) to render a puzzle of the contiguous 48 US states plus Alaska and Hawaii. States are provided and denoted by color, and the counties are scattered within reason around each state. The goal of the puzzle is to drag and drop the counties to their correct positions on the map.
+
+The project is built with JavaScript and React, and it stores progress locally on your machine using browser storage. The puzzle is responsive and works on desktop and mobile devices.
+
 #svg #d3.js #react.js
---
+++ src/components/puzzle/svgUtilities.js
@@ -0,0 +1,51 @@
+import { select, selectAll } from 'd3-selection'
+import { stateId } from '../utilities'
+
+// pass a d3 selection, applys 'located' attributes
+// lowers the county to the bottom then lowers the state to bottom
+const applyLocatedAttr = (selection) => {
+  selection
+    .attr('transform', 'translate(0,0)')
+    .attr('stroke-width', 0.1)
+    .attr('stroke', 'lightgray')
+    .attr('fill', 'slategray')
+    .on('.drag', null)
+    .lower()
+
+  select(`#state-${selection.attr('state-id')}`).lower()
+}
+
+// checks if target's transform values are within threshold to be considered located
+// optionally returns the x, y values as number
+export const transformUtility = (selection, withReturn = true) => {
+  const threshold = 0.75
+  const [x, y] = selection.attr('transform').match(/-?\d+(\.\d+)?/g)
+  if (Math.abs(+x) < threshold && Math.abs(+y) < threshold) {
+    applyLocatedAttr(selection)
+    if (withReturn) return [0, 0]
+  } else if (withReturn) return [+x, +y]
+}
+
+// sets visibility attributes of counties by state
+export const filterStates = (stateFilter) => {
+  selectAll('.county, .state')
+    .style('visibility', 'visible')
+    .attr('pointer-events', 'all')
+    .attr('is-hidden', false)
+  if (stateFilter) {
+    selectAll('.county, .state')
+      .filter(({ id }) => (stateFilter.includes(stateId(id)) ? false : true))
+      .style('visibility', 'hidden')
+      .attr('pointer-events', 'none')
+      .attr('is-hidden', true)
+  }
+}
+// if state filter is selected, update zoom pan pinch to state node coords
+export const updateZoom = (transformRef, stateFilter) => {
+  if (stateFilter) {
+    const node = select(`#state-${stateFilter}`).node()
+    transformRef.current.zoomToElement(node, 2, 500, 'easeOut')
+  } else {
+    transformRef.current.resetTransform(500, 'easeOut')
+  }
+}
